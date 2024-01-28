12ca03adb8347084508f1463073ac20792109385
karimemmonteiro <contatokarimem@gmail.com>
2024-01-28 17:44:43 -0300

    teste 2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,16 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
-const prisma = new PrismaClient();
+// Use path.resolve para obter um caminho absoluto
+const dbFilePath = path.resolve(__dirname, 'prisma', 'dev.db');
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: `file:${dbFilePath}`,
+    },
+  },
+});
 
 export default prisma;
-
