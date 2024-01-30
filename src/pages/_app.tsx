ffcda084e85d31480c276ba16505b65614229655
karimemmonteiro <contatokarimem@gmail.com>
2024-01-30 15:20:16 -0300

    add service

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,20 @@ import { AutenticationsUser } from "../context/AuthContext";
 import Sidebar from "../components/SideBar";
 import "../global.css"
 import Login from "./login";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch((error) => {
+          console.error('Erro ao registrar o Service Worker:', error);
+        });
+    }
+  }, []);
   const pathname = usePathname();
   const isPublicPage = verficicationRouters(pathname)
   const isAutentication = AutenticationsUser()
@@ -15,30 +27,17 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Remoto-pwa</title>
-        <meta name="theme-color" content="#317EFB" />
-        <meta name="description" content="Best PWA app in the world!" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="My awesome PWA app" />
-        <meta name="twitter:description" content="Best PWA app in the world!" />
-        <meta name="twitter:image" content="/icons/twitter.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="My awesome PWA app" />
-        <meta property="og:description" content="Best PWA app in the world!" />
-        <meta property="og:site_name" content="My awesome PWA app" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="/icons/og.png" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#317EFB" />
 
+        <meta name="description" content="Best PWA app in the world!" />
 
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
       </Head>
       {
         isPublicPage ?
