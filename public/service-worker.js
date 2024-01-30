97a44deb4e08ccc3e103e8f4edd530ce7c7dc5ad
karimemmonteiro <contatokarimem@gmail.com>
2024-01-30 16:14:19 -0300

    add nex telas ao pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,55 +1,45 @@
-const CACHE_NAME = 'my-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/manifest.json',
-  '/favicon.ico',
-  '/icons/mask-icon.svg',
-  '/icons/touch-icon-iphone.png',
-  '/icons/twitter.png',
-  '/Sebrae.png',  // Certifique-se de ter o arquivo Sebrae.png na pasta raiz ou ajuste o caminho conforme necessário
-  // Adicione outros recursos estáticos e páginas aqui
-];
+const CACHE_NAME = 'my-cache';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-      .catch((error) => console.error('Erro durante a instalação:', error))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/atendimento',  // Substitua isso pelas URLs reais de suas páginas
+        '/login',
+        // Adicione outras páginas estáticas aqui
+      ]);
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.match(event.request)
-          .then((response) => {
-            return response || fetch(event.request)
-              .then((networkResponse) => {
-                cache.put(event.request, networkResponse.clone());
-                return networkResponse;
-              })
-              .catch((error) => console.error('Erro durante o fetch:', error));
-          })
-          .catch((error) => console.error('Erro durante o match:', error));
-      })
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // Não encontrou no cache - faz a requisição à rede
+      return fetch(event.request).then((response) => {
+        // Verifica se a resposta é válida
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // Clona a resposta para evitar consumir a resposta original
+        const responseToCache = response.clone();
+
+        // Armazena a resposta no cache
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
   );
 });
