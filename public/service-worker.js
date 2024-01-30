33d705c300d5dd29220b550639cf249a1ee958f4
karimemmonteiro <contatokarimem@gmail.com>
2024-01-30 16:09:38 -0300

    ajuste service work

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,55 @@
-const CACHE_NAME = 'my-cache';
+const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  // Adicione outros recursos estáticos aqui
+  '/manifest.json',
+  '/favicon.ico',
+  '/icons/mask-icon.svg',
+  '/icons/touch-icon-iphone.png',
+  '/icons/twitter.png',
+  '/Sebrae.png',  // Certifique-se de ter o arquivo Sebrae.png na pasta raiz ou ajuste o caminho conforme necessário
+  // Adicione outros recursos estáticos e páginas aqui
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+      .catch((error) => console.error('Erro durante a instalação:', error))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.match(event.request)
+          .then((response) => {
+            return response || fetch(event.request)
+              .then((networkResponse) => {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+              })
+              .catch((error) => console.error('Erro durante o fetch:', error));
+          })
+          .catch((error) => console.error('Erro durante o match:', error));
+      })
   );
 });
