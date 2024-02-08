aa2b2f808f63a334b89b54cd6e059d674f42a4d0
karimemmonteiro <contatokarimem@gmail.com>
2024-02-08 18:53:43 -0300

    add pss online

--- config/connection.ts
+++ config/connection.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const APILOCAL = process.env.APILOCAL || "https://localhost:7179/";
+const APILOCAL = process.env.APILOCAL || "https://pss.am.sebrae.com.br/portal-api/";
 const APINEXT = process.env.APINEXT || "http://localhost:3000/api/"
 
 const apiPss = axios.create({
